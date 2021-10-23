524aad94a12bce11f12dfd1b37a48bdabc7e9550
DiegoABorrero <diego.borrero@payu.com>
2021-10-23 17:43:09 -0500

    First Commit

---
+++ src/main/java/co/edu/javeriana/discovery/pica/historic/PicaHistoricApplication.java
@@ -0,0 +1,13 @@
+package co.edu.javeriana.discovery.pica.historic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicaHistoricApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicaHistoricApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/javeriana/discovery/pica/historic/PicaHistoricApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.javeriana.discovery.pica.historic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicaHistoricApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
