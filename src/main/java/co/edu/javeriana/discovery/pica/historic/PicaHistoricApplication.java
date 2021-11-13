63d14a0764a983c1cb68d763b7ac96be04d2868b
Beta993 <81639095+Beta993@users.noreply.github.com>
2021-11-12 20:45:03 -0500

    :sparkles: Se agrega trazabilidad Zipkin

--- src/main/java/co/edu/javeriana/discovery/pica/historic/PicaHistoricApplication.java
+++ src/main/java/co/edu/javeriana/discovery/pica/historic/PicaHistoricApplication.java
@@ -1,8 +1,10 @@
 package co.edu.javeriana.discovery.pica.historic;
 
+import brave.sampler.Sampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -12,4 +14,8 @@ public class PicaHistoricApplication {
 		SpringApplication.run(PicaHistoricApplication.class, args);
 	}
 
+	@Bean
+	public Sampler sampler() {
+		return Sampler.ALWAYS_SAMPLE;
+	}
 }
